2cef5e21fea3580f74ec214b9a5aa3f7efbdebcd
milunax <clemto95@live.fr>
2026-03-09 14:56:16 +0100

    [FIX] BulletLifetime

--- Assets/SpaceInvaderTemplate/Bullets/Bullet.cs
+++ Assets/SpaceInvaderTemplate/Bullets/Bullet.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] Vector3 startVelocity; 
+    [SerializeField] Vector3 startVelocity;
+    [SerializeField] private string destroyCollider;
 
     // Start is called before the first frame update
     void Awake()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.linearVelocity = startVelocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != destroyCollider) return;
+        Destroy(gameObject);
     }
 }
