655b2e3106ef3a6bf07de0b12d6ee22da05ea703
milunax <clemto95@live.fr>
2026-03-11 11:28:36 +0100

    [MOD] bullets particles integration

--- Assets/SpaceInvaderTemplate/Bullets/Bullet.cs
+++ Assets/SpaceInvaderTemplate/Bullets/Bullet.cs
@@ -6,6 +6,10 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] protected Vector3 startVelocity;
     [SerializeField] private string destroyCollider;
+
+    [SerializeField] protected ParticleSystem _loopPS;
+    [SerializeField] protected GameObject _impactPSPrefab;
+
     protected Rigidbody2D _rb;
 
     // Start is called before the first frame update
@@ -15,9 +19,19 @@ public class Bullet : MonoBehaviour
         _rb.linearVelocity = startVelocity;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag != destroyCollider) return;
-        Destroy(gameObject);
+        if (collision.gameObject.tag == "Untagged")
+        {
+            _loopPS.Stop();
+
+            Vector3 dir = collision.transform.position - transform.position;
+
+
+            /*if (_rb.linearVelocity.y >= 0) Instantiate(_impactPSPrefab, transform.position, Quaternion.identity);
+            else */
+            Instantiate(_impactPSPrefab, transform.position, Quaternion.FromToRotation(transform.forward, dir));
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/SpaceInvaderTemplate/Bullets/WeakBullet.cs
+++ Assets/SpaceInvaderTemplate/Bullets/WeakBullet.cs
@@ -1,6 +1,23 @@
-public class WeakBullet : Bullet { private void Start() { _rb.gravityScale = 1.0f; }
+using System.Collections;
+using UnityEngine;
+
+public class WeakBullet : Bullet
+{
+    [SerializeField] private CircleCollider2D Collider2D;
+
+    private float waitTimeCollisionActivation = 0.5f;
+    
+
+    private void Start() 
+    { 
+        _rb.gravityScale = 1.0f;
+    }
+
     private void Update()
     {
+        waitTimeCollisionActivation -= Time.deltaTime;
+        if (waitTimeCollisionActivation < 0) Collider2D.enabled = true;
+
         if(_rb.linearVelocity.y < 0) tag = "Untagged";
     }
 }
