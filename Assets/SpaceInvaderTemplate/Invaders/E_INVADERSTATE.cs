967c582f8775a0bf80391233b6798b910e0da222
Jari Bou <bourdietome@gmail.com>
2026-03-11 16:19:35 +0100

    [ADD] Invader state update

--- Assets/SpaceInvaderTemplate/Invaders/E_INVADERSTATE.cs
+++ Assets/SpaceInvaderTemplate/Invaders/E_INVADERSTATE.cs
@@ -1,8 +1,8 @@
+[System.Flags]
 public enum E_INVADERSTATE
 {
-    NONE = 0,
+    NONE = 0, // Justin Case
     CLEAN = 1,
-    VOMIT = 2,
-    POOP = 4,
-    BOTH = 8
+    VOMIT = 1 << 1,    // 0110 = 6
+    POOP = 1 << 2,
 }
