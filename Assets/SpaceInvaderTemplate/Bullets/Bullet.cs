43037ca15b49ee38e7a26b9d0164e57415d003d3
milunax <clemto95@live.fr>
2026-03-11 14:49:18 +0100

    [FIX] Tweaks on bullets

--- Assets/SpaceInvaderTemplate/Bullets/Bullet.cs
+++ Assets/SpaceInvaderTemplate/Bullets/Bullet.cs
@@ -7,16 +7,31 @@ public class Bullet : MonoBehaviour
     [SerializeField] protected Vector3 startVelocity;
     [SerializeField] private string destroyCollider;
 
+    [Header("Bullet type")]
+    [SerializeField] protected E_INVADERSTATE state;
+
+    [Header("Particle systems")]
     [SerializeField] protected ParticleSystem _loopPS;
     [SerializeField] protected GameObject _impactPSPrefab;
 
+    [Header("Collider")]
+    [SerializeField] protected CircleCollider2D _collider;
+
     protected Rigidbody2D _rb;
+    private float waitTimeCollisionActivation = 0.25f;
+
 
     // Start is called before the first frame update
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _rb.linearVelocity = startVelocity;
+    }
+
+    private void Update()
+    {
+        waitTimeCollisionActivation -= Time.deltaTime;
+        if (waitTimeCollisionActivation < 0) _collider.enabled = true;
     }
 
     protected virtual void OnTriggerEnter2D(Collider2D collision)
--- Assets/SpaceInvaderTemplate/Bullets/WeakBullet.cs
+++ Assets/SpaceInvaderTemplate/Bullets/WeakBullet.cs
@@ -3,11 +3,6 @@ using UnityEngine;
 
 public class WeakBullet : Bullet
 {
-    [SerializeField] private CircleCollider2D Collider2D;
-
-    private float waitTimeCollisionActivation = 0.5f;
-    
-
     private void Start() 
     { 
         _rb.gravityScale = 1.0f;
@@ -15,9 +10,6 @@ public class WeakBullet : Bullet
 
     private void Update()
     {
-        waitTimeCollisionActivation -= Time.deltaTime;
-        if (waitTimeCollisionActivation < 0) Collider2D.enabled = true;
-
         if(_rb.linearVelocity.y < 0) tag = "Untagged";
     }
 }
