c9a4ec951102dafbcba37a411ce7cc7a3ac79f87
Jari Bou <bourdietome@gmail.com>
2026-03-12 19:59:59 +0100

    [FIX] Death animations when not in state

---
+++ Assets/SpaceInvaderTemplate/Scripts/InvaderDeathEventScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpaceInvaderTemplate.Scripts
+{
+    public class InvaderDeathEventScript : MonoBehaviour
+    {
+        private Invader _invaderScript;
+
+        private void Awake()
+        {
+            _invaderScript = transform.parent.GetComponent<Invader>();
+            if (_invaderScript == null)
+            {
+                throw new Exception("Invader Script is missing from parent");
+            }
+        }
+
+        public void Die()
+        {
+            _invaderScript.Die();
+        }
+    }
+}
