162b341ff7f81debff3f8985eb102b13efe0980c
milunax <clemto95@live.fr>
2026-03-12 20:01:46 +0100

    [ADD] Intro video in start screen

---
+++ Assets/SpaceInvaderTemplate/Scripts/IntroVideo/ChangeScene.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer _videoPLayer;
+
+    private AsyncOperation _asyncOperation;
+    
+    private void Start()
+    {
+        _asyncOperation = SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+        _asyncOperation.allowSceneActivation = false;
+
+        _videoPLayer.loopPointReached += OnVideoEnd;
+    }
+
+    private void OnVideoEnd(VideoPlayer source)
+    {
+        _asyncOperation.allowSceneActivation = true;
+    }
+}
